1ee49ec7c6be626241ad5103582c57a5e0839da7
IamUzo <iamuzoagu@gmail.com>
2020-02-12 15:41:05 -0700

    squash this

--- TicTacToe/Models/Strategist.swift
+++ TicTacToe/Models/Strategist.swift
@@ -7,3 +7,30 @@
 //
 
 import GameplayKit
+
+struct Strategist {
+    
+    private let strategist: GKMinmaxStrategist = {
+       let strategist = GKMinmaxStrategist()
+        
+        strategist.maxLookAheadDepth = 5
+        strategist.randomSource = GKARC4RandomSource()
+        
+        return strategist
+    }()
+    
+    var board: Board {
+        didSet {
+            strategist.gameModel = board as? GKGameModel
+        }
+    }
+    
+    var bestCoordinate: CGPoint? {
+        if let move = strategist.bestMove(for: board.currentPlayer) as?
+            Move {
+            return move.coordinate
+        }
+        
+        return nil
+    }
+}
