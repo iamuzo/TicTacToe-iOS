8aeda0f4e37966f0d17dd24cc239bd87d37eb0d2
IamUzo <iamuzoagu@gmail.com>
2020-02-12 16:24:17 -0700

    get started on GameScene

--- TicTacToe/Models/Strategist.swift
+++ TicTacToe/Models/Strategist.swift
@@ -13,19 +13,23 @@ struct Strategist {
     private let strategist: GKMinmaxStrategist = {
        let strategist = GKMinmaxStrategist()
         
+        /**constraints given to strategist to limit the number of moves it can stimulate*/
         strategist.maxLookAheadDepth = 5
         strategist.randomSource = GKARC4RandomSource()
         
         return strategist
     }()
     
+    /**keep reference to game model and supply that to strategist*/
     var board: Board {
         didSet {
-            strategist.gameModel = board as? GKGameModel
+            strategist.gameModel = board
         }
     }
     
+    /**the best coordinate is a CGPoint representing the strategist's best move*/
     var bestCoordinate: CGPoint? {
+        /**returns nil if the play is in aninvalid state or nonexistent*/
         if let move = strategist.bestMove(for: board.currentPlayer) as?
             Move {
             return move.coordinate
